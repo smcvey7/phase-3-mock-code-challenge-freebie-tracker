4d5b054ce909ad41e1b55bfb061b49cfc989242f
Steven McVey <smcvey1210@gmail.com>
2023-01-03 10:28:58 -0800

    commit tree 7bd44d5674c19a0eec182feca51842a2ee69b611

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,13 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
+
+  def give_freebie(dev, item_name, value)
+    Freebie.new(name: item_name, value: value, dev: dev, company: self)
+  end
+
+  def self.oldest_company
+    Company.find_by(founding_year: Company.minimum(:founding_year))
+  end
 
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,21 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    if self.freebies.find_by(item_name: item_name)
+      true
+    else
+      false
+    end
+  end
+
+  def give_away(dev, freebie)
+    if self.received_one?(freebie.name)
+      item = self.freebies.find_by(item_name:freebie.name)
+      item.dev = dev.id
+    else
+      "You don't have a #{freebie.name} to give away"
+  end
 
 end
